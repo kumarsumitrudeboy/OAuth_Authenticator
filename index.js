0b4765429600da8229e414927e5f775b61f01e50
kumarsumitrudeboy <sumitedward3@gmail.com>
2020-10-07 23:17:31 +0530

    Made some changes

--- index.js
+++ index.js
@@ -1,234 +1,102 @@
-var express = require('express');
+var express = require('express'),
+sys = require('sys'),
+util = require('util'),
+OAuth = require('oauth').OAuth,
+fs = require('fs');
+var errorhandler = require('errorhandler');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
 var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var url = require('url');
-var axios = require('axios');
-var passport = require('passport');
-var Saml2js = require('saml2js');
-var SamlStrategy = require('passport-saml').Strategy;
 
-//SAML Strategy
-var saml = passport.use(new SamlStrategy(
-{
-    path: '/login/callback',
-    entryPoint: 'https://sso.connect.pingidentity.com/sso/idp/SSO.saml2?idpid=9caac226-aad4-4f6e-8297-225d8b8917d1',
-    issuer: 'demo-app'
-},
-function(profile, done) {
-    // findByEmail(profile.email, function(err, user) {
-    // if (err) {
-    //     return done(err);
-    // }
-    // return done(null, user);
-    // });
-    //console.log(profile);
-    return done(null, profile);
-})
-);
+// app.configure('development', function() {
+  app.use(errorhandler({ dumpExceptions: true, showStack: true }));
+  app.use(morgan());
+  app.use(cookieParser());
+  app.use(expressSession({secret: "CSB"}));
+// });
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-	done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-	done(null, user);
-});
-
-// create application/json parser
-var jsonParser = bodyParser.json();
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-app.post('/saveConfigure', jsonParser, (req, res) => {
-    console.log('printing req body');
-    console.log(req.body);
-    let oauthDetails = req.body;
-    fs.writeFile('idpConfig.json', JSON.stringify(oauthDetails), ()=>{
-        console.log('IDP Configuration save to idpConfig.json file.');
-    });
-    res.status(200).send('All form submitted');
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/html_files/welcome.html'));
-});
-
-app.get('/authCode', (req, res) => {
-    fs.readFile('./idpConfig.json', (err, data) => {
-        if(!err){
-            let idpData = JSON.parse(data);
-            res.redirect(url.format({
-                pathname: idpData.authEndpoint,
-                query: {
-                    'client_id': idpData.client_id,
-                    'response_type': 'code',
-                    'response_mode': 'query',
-                    'scope': 'openid profile',
-                    'redirect_uri': 'http://localhost:8080/authCodeValidator',
-                    'state': "logged_in",
-                    'nonce': "somerandomstring"
-                }
-            }))
-        }else{
-            res.status(400).send('Bad Request: '+err);
-        }
-    });
-});
-
-app.get('/authCodeValidator', (req, res) => {
-    let code = req.query.code;
-    let state = req.query.state;
-    // || url.parse(req.protocol+'://'+req.hostname+''+req.originalUrl,true, true)
-    //console.log(state);
-    if(state==='logged_in'){
-        if(code!==null||code!==''){
-            fs.readFile('./idpConfig.json', (err, data) => {
-                if(!err){
-                    let idpData = JSON.parse(data);
-                    let client_id = idpData.client_id;
-                    let client_Secret = idpData.client_secret;
-                    let clientCredentials = new Buffer(''+client_id+':'+client_Secret);
-                    let base64ClientCredentials = clientCredentials.toString('base64');
-                    let headers = {
-                        'Authorization': 'Basic '+base64ClientCredentials,
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                        'Connection': 'close',
-                        'Content-Length': '0'
-                    };
-                    // axios.post(idpData.tokenEndpoint, {
-                        // grant_type: 'authorization_code',
-                        // redirect_uri: 'http://localhost:8080/showTokens',
-                        // code: code
-                    // },headers)
-                    axios({
-                        method: 'post',
-                        url: idpData.tokenEndpoint,
-                        params: {
-                            'grant_type': 'authorization_code',
-                            'redirect_uri': 'http://localhost:8080/authCodeValidator',
-                            'code': code
-                        },
-                        headers:{
-                            'Authorization': 'Basic '+base64ClientCredentials,
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/x-www-form-urlencoded',
-                            'Connection': 'close',
-                            'Content-Length': '0' 
-                        }
-                    })
-                    .then((resAxios) => {
-                        console.log(resAxios);
-                        res.status(200).send(resAxios.data);
-                    })
-                    .catch(err => {
-                        console.log(err);
-                        res.status(400).send('Bad Request: '+err);
-                    });
-                }else{
-                    res.status(400).send('Bad Request: '+err);
-                }
-            });
-        }else{
-            res.status(400).send("Invalid Code returned from IDP: "+state);  
-        }
+// var configFile = process.env['USERPROFILE']+"/config.js";
+var config = fs.readFile('./idpConfig.json', (err, data) => {
+    if(!err){
+        let idpData = JSON.parse(data);
+        return idpData;
     }else{
-
-        res.status(400).send("Invalid State: "+state);
+        console.log(err);
     }
 });
+//var config = require(configFile);
 
-app.get('/implicit', (req, res) => {
-    fs.readFile('./idpConfig.json', (err, data) => {
-        if(!err){
-            let idpData = JSON.parse(data);
-            res.redirect(url.format({
-                pathname: idpData.authEndpoint,
-                query: {
-                    'client_id': idpData.client_id,
-                    'response_type': 'token',
-                    'scope': 'openid profile',
-                    'redirect_uri': 'http://localhost:8080/authCodeValidator',
-                    'state': "logged_in",
-                    'nonce': "somerandomstring"
-                }
-            }))
-        }else{
-            res.status(400).send('Bad Request: '+err);
-        }
-    });
+var privateKeyData = config.consumerPrivateKeyFile;
+
+var consumer = 
+  new OAuth("https://jdog.atlassian.com/plugins/servlet/oauth/request-token",
+                  "https://jdog.atlassian.com/plugins/servlet/oauth/access-token",
+                  config["consumerKey"],
+                  "",
+                  "1.0",
+                  "http://localhost:8080/sessions/callback",
+                  "RSA-SHA1",
+				  null,
+				  privateKeyData);
+
+
+
+
+// app.dynamicHelpers({
+//   	session: function(request, response){
+//     	return request.session;
+// 	}
+// });
+
+app.get('/', function(request, response){
+  	response.send('Hello World');
 });
 
-app.get('/clientCredentials', (req, res) => {
-    fs.readFile('./idpConfig.json', (err, data) => {
-        if(!err){
-            let idpData = JSON.parse(data);
-            let client_id = idpData.client_id;
-            let client_Secret = idpData.client_secret;
-            let clientCredentials = new Buffer(''+client_id+':'+client_Secret);
-            let base64ClientCredentials = clientCredentials.toString('base64');
-            axios({
-                method: 'post',
-                url: idpData.tokenEndpoint,
-                params: {
-                    'grant_type': 'client_credentials',
-                    'scope': 'custom_scope_1',
-                },
-                headers:{
-                    'Authorization': 'Basic '+base64ClientCredentials,
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'cache-control': 'no-cache'
-                }
-            })
-            .then((resAxios) => {
-                console.log(resAxios);
-                res.status(200).send(resAxios.data);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(400).send('Bad Request: '+err);
-            });
-        }else{
-            res.status(400).send('Bad Request: '+err);
-        }
-    });
+app.get('/sessions/connect', function(request, response){
+	consumer.getOAuthRequestToken(
+		function(error, oauthToken, oauthTokenSecret, results) {
+    		if (error) {
+				console.log(error.data);
+      			response.send('Error getting OAuth access token');
+			}
+    		else {
+      			request.session.oauthRequestToken = oauthToken;
+      			request.session.oauthRequestTokenSecret = oauthTokenSecret;
+      			response.redirect("https://jdog.atlassian.com/plugins/servlet/oauth/authorize?oauth_token="+request.session.oauthRequestToken);
+			}
+		}
+	)
 });
 
-app.get('/login',
-  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
-  function(req, res) {
-    res.redirect('/');
-  }
-);
+app.get('/sessions/callback', function(request, response){
+	consumer.getOAuthAccessToken (
+			request.session.oauthRequestToken, 
+			request.session.oauthRequestTokenSecret, 
+			request.query.oauth_verifier,
+			function(error, oauthAccessToken, oauthAccessTokenSecret, results){			
+				if (error) { 
+					console.log(error.data);
+					response.send("error getting access token");		
+				}
+    			else {
+      				request.session.oauthAccessToken = oauthAccessToken;
+      				request.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+      				consumer.get("https://jdog.atlassian.com/rest/api/latest/issue/JRADEV-8110.json", 
+						request.session.oauthAccessToken, 
+						request.session.oauthAccessTokenSecret, 
+						"application/json",
+						function(error, data, resp){
+							console.log(data);
+        					data = JSON.parse(data);
+        					response.send("I am looking at: "+data["key"]);
+						}
+					);
+				}
+			}
+		)
+	});
+					
 
-app.get('/home', (req, res) => {
-    let samlResponse;
-    if(req.params.saml){
-        samlResponse = req.params.saml;
-        res.send(samlResponse);
-    }else{
-        res.status(400).send('Bad Request: Missing SAML Response');
-    }
-}
-  
-);
-
-app.post('/login/callback',
-  bodyParser.urlencoded({ extended: false }),
-  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
-  function(req, res) {
-      //console.log(req.user);
-      //console.log(req.body.done);
-        res.json(req.user);
-  }
-);
-
-app.listen(8080, ()=> {
-    console.log('Server is up and listening on port 8080');
-});
+app.listen(parseInt(process.env.PORT || 8080));
---
+++ index2.js
@@ -0,0 +1,234 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var url = require('url');
+var axios = require('axios');
+var passport = require('passport');
+var Saml2js = require('saml2js');
+var SamlStrategy = require('passport-saml').Strategy;
+
+//SAML Strategy
+var saml = passport.use(new SamlStrategy(
+{
+    path: '/login/callback',
+    entryPoint: 'https://pingfedtest.regeneron.com/idp/SSO.saml2',
+    issuer: 'localhost.myapp'
+},
+function(profile, done) {
+    // findByEmail(profile.email, function(err, user) {
+    // if (err) {
+    //     return done(err);
+    // }
+    // return done(null, user);
+    // });
+    //console.log(profile);
+    return done(null, profile);
+})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.post('/saveConfigure', jsonParser, (req, res) => {
+    console.log('printing req body');
+    console.log(req.body);
+    let oauthDetails = req.body;
+    fs.writeFile('idpConfig.json', JSON.stringify(oauthDetails), ()=>{
+        console.log('IDP Configuration save to idpConfig.json file.');
+    });
+    res.status(200).send('All form submitted');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/html_files/welcome.html'));
+});
+
+app.get('/authCode', (req, res) => {
+    fs.readFile('./idpConfig.json', (err, data) => {
+        if(!err){
+            let idpData = JSON.parse(data);
+            res.redirect(url.format({
+                pathname: idpData.authEndpoint,
+                query: {
+                    'client_id': idpData.client_id,
+                    'response_type': 'code',
+                    'response_mode': 'query',
+                    'scope': 'openid profile',
+                    'redirect_uri': 'http://localhost:8080/authCodeValidator',
+                    'state': "logged_in",
+                    'nonce': "somerandomstring"
+                }
+            }))
+        }else{
+            res.status(400).send('Bad Request: '+err);
+        }
+    });
+});
+
+app.get('/authCodeValidator', (req, res) => {
+    let code = req.query.code;
+    let state = req.query.state;
+    // || url.parse(req.protocol+'://'+req.hostname+''+req.originalUrl,true, true)
+    //console.log(state);
+    if(state==='logged_in'){
+        if(code!==null||code!==''){
+            fs.readFile('./idpConfig.json', (err, data) => {
+                if(!err){
+                    let idpData = JSON.parse(data);
+                    let client_id = idpData.client_id;
+                    let client_Secret = idpData.client_secret;
+                    let clientCredentials = new Buffer(''+client_id+':'+client_Secret);
+                    let base64ClientCredentials = clientCredentials.toString('base64');
+                    let headers = {
+                        'Authorization': 'Basic '+base64ClientCredentials,
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Connection': 'close',
+                        'Content-Length': '0'
+                    };
+                    // axios.post(idpData.tokenEndpoint, {
+                        // grant_type: 'authorization_code',
+                        // redirect_uri: 'http://localhost:8080/showTokens',
+                        // code: code
+                    // },headers)
+                    axios({
+                        method: 'post',
+                        url: idpData.tokenEndpoint,
+                        params: {
+                            'grant_type': 'authorization_code',
+                            'redirect_uri': 'http://localhost:8080/authCodeValidator',
+                            'code': code
+                        },
+                        headers:{
+                            'Authorization': 'Basic '+base64ClientCredentials,
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/x-www-form-urlencoded',
+                            'Connection': 'close',
+                            'Content-Length': '0' 
+                        }
+                    })
+                    .then((resAxios) => {
+                        console.log(resAxios);
+                        res.status(200).send(resAxios.data);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(400).send('Bad Request: '+err);
+                    });
+                }else{
+                    res.status(400).send('Bad Request: '+err);
+                }
+            });
+        }else{
+            res.status(400).send("Invalid Code returned from IDP: "+state);  
+        }
+    }else{
+
+        res.status(400).send("Invalid State: "+state);
+    }
+});
+
+app.get('/implicit', (req, res) => {
+    fs.readFile('./idpConfig.json', (err, data) => {
+        if(!err){
+            let idpData = JSON.parse(data);
+            res.redirect(url.format({
+                pathname: idpData.authEndpoint,
+                query: {
+                    'client_id': idpData.client_id,
+                    'response_type': 'token',
+                    'scope': 'openid profile',
+                    'redirect_uri': 'http://localhost:8080/authCodeValidator',
+                    'state': "logged_in",
+                    'nonce': "somerandomstring"
+                }
+            }))
+        }else{
+            res.status(400).send('Bad Request: '+err);
+        }
+    });
+});
+
+app.get('/clientCredentials', (req, res) => {
+    fs.readFile('./idpConfig.json', (err, data) => {
+        if(!err){
+            let idpData = JSON.parse(data);
+            let client_id = idpData.client_id;
+            let client_Secret = idpData.client_secret;
+            let clientCredentials = new Buffer(''+client_id+':'+client_Secret);
+            let base64ClientCredentials = clientCredentials.toString('base64');
+            axios({
+                method: 'post',
+                url: idpData.tokenEndpoint,
+                params: {
+                    'grant_type': 'client_credentials',
+                    'scope': 'custom_scope_1',
+                },
+                headers:{
+                    'Authorization': 'Basic '+base64ClientCredentials,
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'cache-control': 'no-cache'
+                }
+            })
+            .then((resAxios) => {
+                console.log(resAxios);
+                res.status(200).send(resAxios.data);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).send('Bad Request: '+err);
+            });
+        }else{
+            res.status(400).send('Bad Request: '+err);
+        }
+    });
+});
+
+app.get('/login',
+  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/home', (req, res) => {
+    let samlResponse;
+    if(req.params.saml){
+        samlResponse = req.params.saml;
+        res.send(samlResponse);
+    }else{
+        res.status(400).send('Bad Request: Missing SAML Response');
+    }
+}
+  
+);
+
+app.post('/login/callback',
+  bodyParser.urlencoded({ extended: false }),
+  passport.authenticate('saml', { failureRedirect: '/', failureFlash: true }),
+  function(req, res) {
+      //console.log(req.user);
+      //console.log(req.body.done);
+        res.json(req.user);
+  }
+);
+
+app.listen(8080, ()=> {
+    console.log('Server is up and listening on port 8080');
+});
